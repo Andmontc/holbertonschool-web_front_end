71b9c5bef7b59455852038af2d3566d2172428d0
Andmontc <oscarandresmontes@gmail.com>
2021-03-30 11:04:55 -0500

    prime

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,22 @@
+let countPrimeNumbers = _ => {
+  let num = 0
+
+  for (let i = 2; i <= 100; i++) {
+      let prime = true
+
+      for (let j = 2; j < i; j++) {
+          if (i % j === 0) {
+              prime = false
+          }
+      }
+      if (prime) {
+          num += 1
+      }
+  }
+  return num
+}
+
+let now = performance.now()
+countPrimeNumbers()
+let finish = performance.now()
+console.log(`Execution time of printing countPrimeNumbers was ${finish - now} milliseconds.`)
